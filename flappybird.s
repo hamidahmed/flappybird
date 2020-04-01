6a2ddec36223d0c4951cf60670135f1d4ea3f056
Junaid <junaidquraishi.tg@gmail.com>
2020-04-01 00:13:44 -0400

    nothing new

--- flappybird.s
+++ flappybird.s
@@ -6,26 +6,45 @@
 # - Display width in pixels: 256
 # - Display height in pixels: 256
 # - Base Address for Display: 0x10008000 ($gp)
-#j = $s0
+#
 .data
 	displayAddress:	.word	0x10008000
+	newline: .asciiz "\n"
+	birdPos: .word 2048, 1920, 2176
+	
 .text
 	lw $t0, displayAddress	# $t0 stores the base address for display
-	li $t1, 0xff0000	# $t1 stores the red colour code
-	li $t2, 0xffff00	# $t2 stores the yellow colour code
-	li $t3, 0x0000ff	# $t3 stores the blue colour code
-	li $t4, 0x86c5da         #stores the light blue color
+	li $t1, 0xFFE945	# $t1 stores the yellow colour code
+	li $t2, 0x2DA617	# $t2 stores the green colour code
+	li $t3, 0x9AFCF6	# $t3 stores the blue colour code
+	li $t4, 1		# $t4 stores if the bird is still alive
 	
+	#sw $t1, 0($t0)	 # paint the first (top-left) unit red. 
+	#sw $t2, 4($t0)	 # paint the second unit on the first row green. Why $t0+4?
+	#sw $t3, 8($t0)	 # paint the second unit on the first row green. Why $t0+4?
+	#sw $t2, 4092($t0) # paint the first unit on the second row blue. Why +128?
 	
-	li $s0,0
-	loop:
-	bgt $s0,1023,Exit
-	addi $s0,$s0,1
-	sw $t4, ($t0)    #paint the Display
-	addiu $t0,$t0,4
-	j loop
-	
-	
+	LOOPINIT:
+		li $s0, 0
+	WHILE:
+		subi $s1, $s0, 4092
+		bgtz $s1, DONE
+		
+		# prints 01234...(N-1), Hence loops N times
+		#li $v0, 1
+		#move $a0, $s0
+		#syscall
+		
+		#main body------------------------------------
+		sw $t3, 0($gp)
+		#---------------------------------------------
+		
+		addi $s0, $s0, 4
+		addi $gp, $gp, 4
+		
+		j WHILE
+	DONE:	
+		j Exit
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
